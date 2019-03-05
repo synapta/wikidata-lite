ec9fa60fb28068c4cba0c7ccbe531d5e2b782aba
alemela <alessiom92@gmail.com>
2019-03-05 10:16:51 +0100

    Minor improvements

--- wd-lite.sh
+++ wd-lite.sh
@@ -6,18 +6,20 @@
 # trova P31
 # filtra Q giuste
 
-echo "Finding data"
-time pigz -dc wd-nt.gz | grep -E -e "entity/Q[0-9]+> <http://schema.org/name>.*@it" \
-                                 -e "Q[0-9]+> <http://schema.org/description>.*@it" \
-                                 -e "<http://www.wikidata.org/prop/direct/P31>" >> temp.nt
+#yq r example.yaml predicate-object
 
-echo "Sorting"
-time sort -u temp.nt > utemp.nt
+inputfile="$1"
+
+echo "Finding data"
+time pigz -dc $inputfile | grep -E -e "/entity/Q[0-9]+> <http://schema.org/name>.*@it" \
+                                 -e "/entity/Q[0-9]+> <http://schema.org/description>.*@it" \
+                                 -e "<http://www.wikidata.org/prop/direct/P31>" \
+                       | sort -u > utemp.nt
 
 echo "Cleaning"
 findq='^<http:\/\/www.wikidata.org\/entity\/(Q[0-9]+)'
-findname=' <http:\/\/schema.org\/name> '
-findP31=' <http:\/\/www.wikidata.org\/prop\/direct\/P31> '
+findname=' <http://schema.org/name> '
+findP31=' <http://www.wikidata.org/prop/direct/P31> '
 currentq=''
 currentdata=''
 haslabel=false
@@ -26,37 +28,38 @@ hasP31=false
 
 while read p; do
     #find current Q
-    if [[ $p =~ $findq ]]; then #XXX can be done better with string split
+    #XXX can be done better with string split
+    #thisq="$(echo $p | perl -pe 's|(.*)(Q[0-9]+)> <(.*)|\2|')" #sed -E  's/(.*)(Q[0-9]+)> <(.*)/\2/'
+    if [[ $p =~ $findq ]]; then
         #check if new Q
         if [[ $currentq != ${BASH_REMATCH[1]} ]]; then
             #set new Q
             currentq=${BASH_REMATCH[1]};
             #echo $currentq
-            
+
             #print previous if necessary
-            if [ $haslabel == true ] && [ $hasP31 == true ]; then 
+            if [ $haslabel == true ] && [ $hasP31 == true ]; then
                 echo -e $currentdata >> output.nt
             fi
-            
-            #clean vars
-			currentdata=''
-			haslabel=false
-			hasP31=false
-        fi
-        
-		#get property switch case
-		if [[ $p =~ $findname ]]; then
-			haslabel=true
-		elif [[ $p =~ $findP31 ]]; then
-			hasP31=true
-		fi
 
-		#save data
-		currentdata="$currentdata$p\n"
+            #clean vars
+    		  	currentdata=''
+    			  haslabel=false
+    			  hasP31=false
+        fi
+
+    		#get property switch case
+    		if [[ $p == *$findname* ]]; then
+    		  	haslabel=true
+    		elif [[ $p == *$findP31* ]]; then
+    		  	hasP31=true
+    		fi
+
+    		#save data
+    		currentdata="$currentdata$p\n"
     fi
 done < utemp.nt
 
-rm temp.nt
 rm utemp.nt
 
 
