42ee557fc084ac88622c2f707afd642993d32adc
Diego Monti <diegmonti@gmail.com>
2020-03-15 08:40:06 +0100

    Fix missing results

--- lite.py
+++ lite.py
@@ -1,9 +1,9 @@
 import argparse
-import mgzip
 import orjson
 import yaml
 
 from joblib import Parallel, delayed
+from xopen import xopen
 
 # TODO create lookup file
 sitelinks = {
@@ -97,49 +97,53 @@ def process_line(line):
 
     entity = orjson.loads(line)
 
-    result = []
+    accumulator = []
 
     try:
-        run_recipe(entity, result, 'labels')
-        run_recipe(entity, result, 'descriptions')
-        run_recipe(entity, result, 'aliases')
-        run_recipe(entity, result, 'sitelinks')
-        run_recipe(entity, result, 'claims')
-        run_recipe(entity, result, 'filters')
+        run_recipe(entity, accumulator, 'labels')
+        run_recipe(entity, accumulator, 'descriptions')
+        run_recipe(entity, accumulator, 'aliases')
+        run_recipe(entity, accumulator, 'sitelinks')
+        run_recipe(entity, accumulator, 'claims')
+        run_recipe(entity, accumulator, 'filters')
     except UserWarning:
         return
 
-    for triple in result:
-        subject = 'wd:' + triple[0]
+    lines_out = []
 
-        if triple[1] == "labels":
+    for assertion in accumulator:
+        subject = 'wd:' + assertion[0]
+
+        if assertion[1] == "labels":
             predicate = "schema:name"
-        elif triple[1] == "descriptions":
+        elif assertion[1] == "descriptions":
             predicate = "schema:description"
-        elif triple[1] == "aliases":
+        elif assertion[1] == "aliases":
             predicate = "skos:altLabel"
-        elif triple[1] == "sitelinks":
+        elif assertion[1] == "sitelinks":
             predicate = "schema:about"
         else:
-            predicate = "wdt:" + triple[1]
+            predicate = "wdt:" + assertion[1]
 
-        if triple[2][0] == 'Q':
-            obj = 'wd:' + triple[2]
+        if assertion[2][0] == 'Q':
+            obj = 'wd:' + assertion[2]
         else:
-            obj = triple[2]
+            obj = assertion[2]
 
-        if triple[1] == 'sitelinks':
-            line = obj + ' ' + predicate + ' ' + subject + ' .'
+        if assertion[1] == 'sitelinks':
+            line_out = obj + ' ' + predicate + ' ' + subject + ' .'
         else:
-            line = subject + ' ' + predicate + ' ' + obj + ' .'
+            line_out = subject + ' ' + predicate + ' ' + obj + ' .'
 
-        return line
+        lines_out.append(line_out)
+
+    return lines_out
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Wikidata lite')
     parser.add_argument('recipe', help='The YAML recipe file')
-    parser.add_argument('input', help='The gzipped Wikidata JSON dump')
+    parser.add_argument('input', help='The Wikidata JSON dump')
     parser.add_argument('output', help='The TTL output file')
     parser.add_argument('--n_jobs', type=int, default=2, help='The number of workers')
     parser.add_argument('--verbose', type=int, default=50, help='The verbosity level')
@@ -151,13 +155,13 @@ if __name__ == "__main__":
 
     # TODO validate recipe
 
-    with mgzip.open(args.input, 'rt') as fp_in, open(args.output, 'w') as fp_out:
+    with xopen(args.input, 'rt') as fp_in, xopen(args.output, 'w') as fp_out:
         fp_out.write("@prefix wd: <http://www.wikidata.org/entity/> .\n")
         fp_out.write("@prefix wdt: <http://www.wikidata.org/prop/direct/> .\n")
         fp_out.write("@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n")
         fp_out.write("@prefix skos: <http://www.w3.org/2004/02/skos/core#> .\n")
 
-        for triple in Parallel(n_jobs=args.n_jobs, verbose=args.verbose)(
+        for triples in Parallel(n_jobs=args.n_jobs, verbose=args.verbose)(
                 delayed(process_line)(raw_line) for raw_line in fp_in):
-            if triple is not None:
-                fp_out.write(triple + '\n')
+            if triples is not None:
+                [fp_out.write(triple + '\n') for triple in triples]
